7c9b4bcaf3e96716b4dd21dfc5c23a244ee3ca77
Domenica Montesdeoca <dome.mont@hotmail.com>
2026-07-10 22:48:53 -0500

    Updated README and Pseudocode

---
+++ 5_app/App.py
@@ -0,0 +1,26 @@
+"""
+content = pseudo code
+course  = Python Advanced
+ 
+author = Domenica Montesdeoca
+date    = 10/07/2026
+"""
+
+# Get selected item relatives
+# Separate them in different list according of what they are
+# Highlight them or show them in writing
+
+# OPTIONS
+# a) Be able to select them in the choosen format and go to them in the outliner, node window or attribute editor
+# b) Save the information of the system to be used later, either for recreating the system or for making comparitions
+
+# Extra idea
+# For loop in joints and controls to look for:
+# - Incorrect orientations: 
+#		That joints are oriented to their child and that controls are alignet to their respective joints
+# - Double transformations:
+#		Look for overlapping constraints or connections
+# - Lack of frozen transformation in gepmetry
+#
+# Look for unused nodes
+# Choose to delete or keep them
--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# PyAdvanced
+# The final idea for the app hasn’t been decided, but all of them go around the concept of being able to understand rigs better. Get to know what connections are used (constraints, nodes,…) either to use this information to debug the rig, clean it or be able to replicate it.
+
+# 
+
+# The app will be for Autodesk Maya.
+
+# 
+
